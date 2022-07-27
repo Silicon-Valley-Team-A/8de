63cfcfc1701bc470677c903f91abceece8db28d5
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-07-27 16:31:19 +0900

    음악 개수 20곡 한도

--- server/music/views.py
+++ server/music/views.py
@@ -15,7 +15,7 @@ from server.settings import BASE_DIR, get_secret, INTERNAL_HOST_IP
 # Create your views here.
 
 ### 노래 개수가 20개를 넘지 않도록 함 ###
-NEED_MORE = True
+IS_FULL = False
 
 ### 노래 검색 결과를 적절한 형식에 맞게 변환 ###
 def search_songs(results):
@@ -67,23 +67,23 @@ def music_classification(sp, data, res, key):
         result = get_features(sp, classify['id'])
         cnt += 1
         if(key['mood']=="happy"):
-            if(result['danceability']>0.7 and result['energy']>0.7 and result['valence']>0.7 and result['tempo']>100):
+            if(result['danceability']>0.7 or result['energy']>0.7 or result['valence']>0.7 or result['tempo']>100):
                 res['music'].append(classify)
             else:
                 pass
         elif(key['mood']=="sad"):
-            if(result['danceability']<0.4 and result['energy']<0.4 and result['valence']<0.4 and result['tempo']<90):
+            if(result['danceability']<0.4 or result['energy']<0.4 or result['valence']<0.4 or result['tempo']<90):
                 res['music'].append(classify)
             else:
                 pass
         elif(key['mood']=="scary"):
-            if(result['danceability']<0.7 and result['danceability']>0.4 and result['energy']>0.7 and result['valence']<0.4 and result['tempo']>100):
+            if(result['danceability']<0.7 or result['danceability']>0.4 or result['energy']>0.7 or result['valence']<0.4 or result['tempo']>100):
                 res['music'].append(classify)
             else:
                 pass
-
-    if (len(res['music']) >= 20):
-        NEED_MORE = False
+        if (len(res['music']) >= 20):
+            IS_FULL = True
+            break
     return res
 
 
@@ -91,7 +91,7 @@ def music_classification(sp, data, res, key):
 def music(request):
     if request.method == "POST":
         # 전역변수 설정
-        NEED_MORE = True
+        IS_FULL = False
 
         ### 이미지 파일 데이터베이스에 저장 ###
         upload = request.FILES.get("upload_image")
@@ -132,16 +132,8 @@ def music(request):
                 results = sp.search(q=key['keyword'][i]+" genre: "+genre, limit=50)
             data = search_songs(results) # 검색 결과를 우선 형식에 맞게 변환하여 data로 저장
             res = music_classification(sp, data, res, key) # data에서 분위기에 맞는 음악만을 따로 추출해서 res에 저장
-
-
-        ### 플레이리스트 길이가 너무 적으면 추가 플레이리스트 추출 ###
-        # if(NEED_MORE):
-        #     if(genre == ''):
-        #         results = sp.search(q=key['mood'], limit=50)
-        #     else:
-        #         results = sp.search(q=key['mood']+" genre: "+genre, limit=50)    
-        #     data = search_songs(results)
-        #     res = music_classification(sp, data, res, key)
+            if IS_FULL:
+                break
 
 
         res['status'] = "success" # 성공/실패 여부
